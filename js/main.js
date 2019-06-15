75d650e04c0951407c02a55a1b92db8100b1f1c2
Artser <artserlisa@mail.ru>
2019-06-15 08:25:28 +0300

    150608

--- js/main.js
+++ js/main.js
@@ -1,83 +1,63 @@
 'use strict';
-(function () {
-  var arrayRealty = [
-    {
-      title: [
-        'Большая уютная квартира',
-        'Маленькая неуютная квартира',
-        'Огромный прекрасный дворец',
-        'Маленький ужасный дворец',
-        'Красивый гостевой домик',
-        'Некрасивый негостеприимный домик',
-        'Уютное бунгало далеко от моря',
-        'Неуютное бунгало по колено в воде'
-      ]
-    },
-    // {
-    //   author: {
-    //     avatar: 'img/avatars/user0' + i + '.png'
-    //   }
-    // },
-    {
-      offer: ['flat', 'house', 'bungalo']
-    }
-  ];
-  // {
-  // for (var i = 1; i <= 8; i++) {
-  //  var locationX = generateRandomNumber(0, 0);
-  //  var locationY = generateRandomNumber(130, 630);
-  // }
 
-  var data = [
-    {
-      author: {
-        avatar: 'img/avatars/user01.png'
-      },
-      offer: {
-        title: 'flat'
-      },
-      location: {
-        x: 300,
-        y: 300
-      }
-    }
-  ];
-  // //
-  // // функция генерации случайных данных
-  // var getRandomInteger = function (min, max) {
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // };
-  // // DOM-элементы, соответствующие меткам на карте
-  // var similarPinsElement = document.querySelector('.map__pins');
-  // var similarPinTemplate = document
-  //   .querySelector('template')
-  //   .content.querySelector('.map__pin');
+var types = ['flat', 'house', 'bungalo', 'palace'];
+// Создадим функцию для генерации номера индекса
+var getRandomOffer = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
-  // var renderPin = function (notice) {
-  //   var pinElement = similarPinTemplate.cloneNode(true);
-  //   var pinX = notice.location.x - 65;
-  //   var pinY = notice.location.y - 65;
-  //   pinElement.style = 'left: ' + pinX + 'px; top: ' + pinY + 'px;';
-  //   pinElement.querySelector('.map__pin img').src = notice.author.avatar;
-  //   pinElement.querySelector('.map__pin img').alt = notice.offer.title;
-  //   return pinElement;
-  // };
-  var drawPin = function (template, dataobj) {
-    var pin = template.cloneNode(true);
-    var pinWidth = 46;
-    var pinHeight = 62;
-    pin.style.left = dataobj.location.x + pinWidth / 2 + 'px';
-    pin.style.top = dataobj.location.y + pinHeight + 'px';
-    pin.querySelector('img').src = dataobj.author.avatar;
-    pin.querySelector('img').alt = 'Большая уютная квартира';
+var getNumberOfRange = function (minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue) + minValue);
+};
 
-    return pin;
-  };
-
+var showMap = function () {
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
+};
+
+// Создадим функцию генерации меток
+var generateLabels = function (quantity) {
+  var adverts = [];
+  for (var i = 1; i <= quantity; i++) {
+    // заполняем массив объектами
+    adverts.push({
+      autor: {
+        avatar: 'img/avatars/user0' + i + '.png'
+      },
+      offer: {
+        type: getRandomOffer(types)
+      },
+      location: {
+        x: getNumberOfRange(0, 1200),
+        y: getNumberOfRange(130, 630)
+      }
+    });
+  }
+  return adverts;
+};
+
+var drawPin = function (dataobj) {
+  var PIN_WIDTH = 62;
+  var PIN_HEIGHT = 84;
+  var template = document
+    .querySelector('#pin')
+    .content.querySelector('.map__pin'); // ищем тег template и берем всего содержимое
+  var pin = template.cloneNode(true);
+  pin.style.left = dataobj.location.x - PIN_WIDTH / 2 + 'px';
+  pin.style.top = dataobj.location.y - PIN_HEIGHT + 'px';
+  pin.querySelector('img').src = dataobj.author.avatar;
+  return pin;
+};
+
+var drawPins = function (data) {
+  var mapPinsElement = document.querySelector('.map__pins');
   var pinsFragment = document.createDocumentFragment();
-  var template = document.querySelector('#pin').content; // ищем тег template и берем всего содержимое
-  var pinTemplate = template.querySelector('.map__pin'); // ищем элемент
-  pinsFragment.appendChild(drawPin(pinTemplate, data[0])); // обращаемся к созданному элементу pinsFragment и создем потомков
-})();
+  data.forEach(function (obj) {
+    pinsFragment.appendChild(drawPin(obj));
+  });
+  mapPinsElement.appendChild(pinsFragment);
+};
+
+var ads = generateLabels(8);
+drawPins(ads);
+showMap();
